898241bc6884078780ff213ec7e25f3235e249df
Anton Brok-Volchansky <brokvolchansky@gmail.com>
2025-08-07 21:01:19 +0200

    feat: changed nice_view to nice_view_gem

--- build.sh
+++ build.sh
@@ -7,15 +7,29 @@ CONTAINER_ID=$(docker ps -q -f "volume=/workspaces/zmk")
 
 echo "Using container: $CONTAINER_ID"
 
+# Копируем west.yml в config папку контейнера
+echo "Copying west.yml to container config folder..."
+docker cp ./west.yml $CONTAINER_ID:/workspaces/zmk-config/config/west.yml
+
+# Инициализация west если еще не инициализирован
+echo "Checking west initialization..."
+docker exec -w /workspaces/zmk-config $CONTAINER_ID bash -c "if [ ! -f /workspaces/zmk-config/.west/config ]; then west init -l /workspaces/zmk-config/config; fi"
+
+# Обновление модулей (включая nice-view-gem)
+echo "Updating west modules..."
+docker exec -w /workspaces/zmk-config $CONTAINER_ID west update
+
 echo "The left one..."
 docker exec -w /workspaces/zmk/app $CONTAINER_ID west build -p -d build/left -b nice_nano_v2 -- \
-  -DSHIELD="corne_left nice_view_adapter nice_view" \
-  -DZMK_CONFIG=/workspaces/zmk-config/config
+  -DSHIELD="corne_left nice_view_adapter nice_view_gem" \
+  -DZMK_CONFIG=/workspaces/zmk-config/config \
+  -DZMK_EXTRA_MODULES="/workspaces/zmk-config/modules/shields/nice_view_gem"
 
 echo "The right one..."
 docker exec -w /workspaces/zmk/app $CONTAINER_ID west build -p -d build/right -b nice_nano_v2 -- \
-  -DSHIELD="corne_right nice_view_adapter nice_view" \
-  -DZMK_CONFIG=/workspaces/zmk-config/config
+  -DSHIELD="corne_right nice_view_adapter nice_view_gem" \
+  -DZMK_CONFIG=/workspaces/zmk-config/config \
+  -DZMK_EXTRA_MODULES="/workspaces/zmk-config/modules/shields/nice_view_gem"
 
 echo "Settings reset..."
 docker exec -w /workspaces/zmk/app $CONTAINER_ID west build -p -d build/reset -b nice_nano_v2 -- \
